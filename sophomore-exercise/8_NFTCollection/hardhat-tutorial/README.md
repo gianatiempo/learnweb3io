901ca88c6db9825fd233f75f24d63196e6947d91
Ariel Gianatiempo <gianatiempo@gmail.com>
2022-05-22 19:59:05 -0300

    sophomore 8

---
+++ sophomore-exercise/8_NFTCollection/hardhat-tutorial/README.md
@@ -0,0 +1,5 @@
+# Basic Sample Hardhat Project
+
+```
+Crypto Devs Contract Address: 0xcB98D17DdB6bAdD22465a82827639B301116980d
+```
---
+++ sophomore-exercise/8_NFTCollection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x95A9065c584DF667ca61946f98499e7E4b429F8e";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://learnweb3io-7exbhbf5a-gianatiempo.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
