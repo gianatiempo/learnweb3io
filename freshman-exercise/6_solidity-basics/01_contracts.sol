c3b7b5b0ce245654e6f6cdb96dfb53de36ef4db6
Ariel Gianatiempo <gianatiempo@gmail.com>
2022-05-22 17:14:45 -0300

    freshman 6

---
+++ freshman-exercise/6_solidity-basics/01_contracts.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier:MIT
+// Define the compiler version you would be using
+pragma solidity ^0.8.10;
+
+// Start by creating a contract named HelloWorld
+contract HelloWorld {
+
+}
